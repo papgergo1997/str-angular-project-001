6786d8dfd1fdd528877673efe868f8a751864adb
czetoj <czeto.jozsef.0229@gmail.com>
2021-01-28 14:39:13 +0100

    model és service osztályok

---
+++ src/app/model/product.ts
@@ -0,0 +1,27 @@
+export class Product {
+    id: number = 0;
+    catId: number = 0;
+    name: string = '';
+    description: string = '';
+    image: string = '';
+    price: number = 0;
+    salesPrice: number = 0;
+    stock: number = 0;
+    featured: boolean = false;
+    active: boolean = true;
+
+    constructor(properties?: Product) {
+        if (properties) {
+            this.id = properties.id || 0;
+            this.catId = properties.catId || 0;
+            this.name = properties.name || '';
+            this.description = properties.description || '';
+            this.image = properties.image || '';
+            this.price = properties.price || 0;
+            this.salesPrice = properties.salesPrice || 0;
+            this.stock = properties.price || 0;
+            this.featured = properties.featured || false;
+            this.active = properties.active || true;
+        }
+    }
+}
