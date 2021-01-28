371690d72ab8f5f83dd4c45928ba6a61419be253
papgergo1997 <pap.gergo.1997@gmail.com>
2021-01-28 19:31:46 +0100

    product-card-component-ts-modification-in-dev-branch-was-needed

--- src/app/common/product-card/product-card.component.ts
+++ src/app/common/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-//import { Product } from 'src/app/model/product';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-product-card',
