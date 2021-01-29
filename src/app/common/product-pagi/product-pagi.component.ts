35621e957283fd407cef8964b4cb1d957db0778d
pinprojekt <pin.janos@gmail.com>
2021-01-29 07:24:50 +0100

    component pagi and cat01 search?

---
+++ src/app/common/product-pagi/product-pagi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-pagi',
+  templateUrl: './product-pagi.component.html',
+  styleUrls: ['./product-pagi.component.scss']
+})
+export class ProductPagiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
