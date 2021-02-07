381c6d8d407c1317822c8f8437c614f5e9906b9d
papgergo1997 <pap.gergo.1997@gmail.com>
2021-02-07 12:53:54 +0100

    Json server added, everything works fine except the pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
 import { Cat01Component } from './page/cat01/cat01.component';
 import { Cat02Component } from './page/cat02/cat02.component';
 import { HomeComponent } from './page/home/home.component';
+import { FilterPipe } from './pipe/filter.pipe';
 import { ProductCardComponent } from './common/product-card/product-card.component';
 import { ProductPagiComponent } from './common/product-pagi/product-pagi.component';
-import { EmployeeCardComponent } from './common/employee-card/employee-card.component';
-import { ProductListComponent } from './common/product-list/product-list.component';
-import { AdminComponent } from './common/admin/admin.component';
-import { NumberHuPipe } from './pipe/number-hu.pipe';
-import { FilterPipe } from './pipe/filter.pipe';
 import { SortPipe } from './pipe/sort.pipe';
+import { EmployeeCardComponent } from './common/employee-card/employee-card.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductListComponent } from './common/product-list/product-list.component';
+import { NumberHuPipe } from './pipe/number-hu.pipe';
+import { KapcsolatComponent } from './common/kapcsolat/kapcsolat.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './common/admin/admin.component';
+import { FormsModule } from '@angular/forms';
 import { CategoryPipe } from './pipe/category.pipe';
 import { FeaturedPipe } from './pipe/featured.pipe';
 import { SalesPipe } from './pipe/sales.pipe';
@@ -39,10 +40,11 @@ import { SalesPipe } from './pipe/sales.pipe';
     FooterComponent,
     ProductListComponent,
     NumberHuPipe,
+    KapcsolatComponent,
     AdminComponent,
     CategoryPipe,
     FeaturedPipe,
-    SalesPipe,
+    SalesPipe
   ],
   imports: [
     BrowserModule,
