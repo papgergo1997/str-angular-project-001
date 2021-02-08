1baf9d5544d48b50eeb21b4814955306dd389bef
papgergo1997 <pap.gergo.1997@gmail.com>
2021-02-08 13:12:17 +0100

    universal filter pipe létrehozva az admin felület e szerint szűrhető

--- src/app/common/data-editor/data-editor.component.ts
+++ src/app/common/data-editor/data-editor.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from 'src/app/model/product';
 import { ITableCol } from 'src/app/service/config-table.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-data-editor',
@@ -8,9 +10,11 @@ import { ITableCol } from 'src/app/service/config-table.service';
   styleUrls: ['./data-editor.component.scss']
 })
 export class DataEditorComponent implements OnInit {
-
+  phrase: string = '';
+  productKeys: string[] = Object.keys(new Product());
   search: string = '';
   filterKey: string = '';
+  productList$: Observable<Product[]> = this.productService.getAll();
   @Input() cols: ITableCol[] = [];
   @Input() productList: Product[] = [];
   @Output() updateProduct: EventEmitter<Product> = new EventEmitter()
@@ -18,15 +22,15 @@ export class DataEditorComponent implements OnInit {
   @Output() createProduct: EventEmitter<Product> = new EventEmitter()
 
   constructor(
-
+    private productService: ProductService,
   ) { }
 
   ngOnInit(): void {
   }
-  onChangeSearch(event: Event, filterKey: string): void {
-    this.search = (event.target as HTMLInputElement).value;
-    this.filterKey = filterKey;
-  }
+  // onChangeSearch(event: Event, filterKey: string): void {
+  //   this.search = (event.target as HTMLInputElement).value;
+  //   this.filterKey = filterKey;
+  // }
 
   onUpdateClick(product: Product): void {
     this.updateProduct.emit(product)
